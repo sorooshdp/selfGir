15d634ba4ab3499d0605467ea10f8a2bb084dd12
soroosh <sorooshdpki@gmail.com>
2025-03-19 02:04:02 +0330

    refactor: remove unnecessary files and move usernames and passwords to seperate module

--- selfGir.js
+++ selfGir.js
@@ -1,13 +1,5 @@
 const puppeteer = require("puppeteer-core");
-
-const users = {
-    soroosh: { userName: "1400012268022", pass: "4311745214" },
-    saeed: { userName: "1400010121101", pass: "2700326970" },
-    mahan: { userName: "1400012137020", pass: "0150329075" },
-    aryan: { userName: "1401012137052", pass: "2581513381" },
-    amirreza: { userName: "1400012137028", pass: "9119012218" },
-    sina: { userName: "1402121320020", pass: "6660590978" },
-};
+import { users } from "./users.js";
 
 async function typeIntoInput(page, selector, text) {
     await page.waitForSelector(selector);
@@ -34,14 +26,12 @@ async function reserveFood(user, num) {
             await pages[0].close();
         }
         await page.goto("https://samad.app/login");
-        await new Promise((resolve) => setTimeout(resolve, 4000));
+        await new Promise((resolve) => setTimeout(resolve, 6000));
 
         await page.click(".input-btn-edit");
 
-        // Wait for the list to appear
         await page.waitForSelector("ul.select-none");
 
-        // Find the element with the text "دانشگاه گیلان" and click it
         await page.evaluate(() => {
             const options = Array.from(document.querySelectorAll("ul.select-none > li"));
             const gilanOption = options.find((option) => option.textContent.trim() === "دانشگاه گیلان");
@@ -74,7 +64,6 @@ async function reserveFood(user, num) {
 
         const parentSelector = ".icon";
 
-        // Wait for the icon to be present before clicking
         await page.waitForSelector(parentSelector);
 
         await page.evaluate(() => {
@@ -82,7 +71,6 @@ async function reserveFood(user, num) {
             console.log("group id clicked");
         });
 
-        // Wait for the select dropdown to be present before interacting with it
         await page.waitForSelector("#selectself_selfListId");
         await page.select("#selectself_selfListId", "1");
         await page.click('input[value="تایید و ادامه"]');
@@ -91,15 +79,13 @@ async function reserveFood(user, num) {
         let optionReserved = false;
         let optionAvailable = false;
 
-        // for handling the alert after clicking the button
         page.on("dialog", async (dialog) => {
             await dialog.accept();
         });
 
         while (!optionReserved) {
-            // Wait for the reservation section to be present before evaluating
             try {
-                await new Promise((resolve) => setTimeout(resolve, 2000));
+                await new Promise((resolve) => setTimeout(resolve, 404));
 
                 optionAvailable = await page.evaluate((select) => {
                     const reservationSection = document.getElementById(
@@ -109,38 +95,20 @@ async function reserveFood(user, num) {
                 }, num);
 
                 if (optionAvailable) {
-                    // Click on the reservation section and submit
                     await page.evaluate((select) => {
                         const reservationSection = document.getElementById(
                             `buyFreeFoodIconSpanuserWeekReserves.selected${select}`
                         ).firstElementChild;
                         reservationSection.click();
 
-                        document.getElementById("doReservBtn").click;
+                        const submit = document.getElementById("doReservBtn");
+                        submit.click();
                     }, num);
                 } else {
-                    // await page.evaluate(async (num) => {
-                    //     const response = await fetch(window.location.href, {
-                    //         method: "GET",
-                    //         headers: {
-                    //             "Content-Type": "text/html",
-                    //         },
-                    //     });
-                    //     const text = await response.text();
-                    //     const parser = new DOMParser();
-                    //     const doc = parser.parseFromString(text, "text/html");
-                    //     const newReservations = doc.getElementById(`buyFreeFoodIconSpanuserWeekReserves.selected${num}`);
-                    //     if (newReservations) {
-                    //         document.getElementById(`buyFreeFoodIconSpanuserWeekReserves.selected${num}`).innerHTML = newReservations.innerHTML;
-                    //     } else {
-                    //         console.log("no update!");
-                    //     }
-                    // }, num);
                     refreshCount++;
                     console.log(`refreshing for ${user.userName} count: ${refreshCount}`);
                     await page.reload();
                 }
-                // Wait for success message to be present before checking
                 optionReserved = await page.evaluate((select) => {
                     const successMassage = document.getElementById(select);
                     return successMassage !== null;
@@ -156,4 +124,4 @@ async function reserveFood(user, num) {
     }
 }
 
-reserveFood(users.sina, 3);
+reserveFood(users.mahan, 5)
--- test.js
+++
@@ -1,7 +0,0 @@
-// for (var i = 0; i < 10; i++) {
-//     setTimeout(() => {
-//         console.log(`the value of i is ${i}`);
-//     }, 10000)
-// }
-
-
